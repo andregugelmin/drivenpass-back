391a8229baa249b58438c5e81552108c17e0405e
isabellacgmsa <isabellacgmsa1@gmail.com>
2022-08-04 18:25:20 -0300

    Preparando revisão de código

--- src/schemas/credentialSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-import { CreateCredentialData } from '../services/credentialService.js';
-
-export const credentialSchema = Joi.object<CreateCredentialData>({
-    title: Joi.string().required(),
-    site: Joi.string().required(),
-    username: Joi.string().required(),
-    password: Joi.string().required(),
-});
